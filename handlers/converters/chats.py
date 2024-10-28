3314eb863ce1f0934c55512eff5b1896ad340160
falcolnic <mykh.yano@gmail.com>
2024-10-29 01:40:28 +0200

    initial commit with command

---
+++ handlers/converters/chats.py
@@ -0,0 +1,14 @@
+from dtos.messages import ChatListItemDTO
+
+from telegram.helpers import escape_markdown
+
+
+def convert_chats_dtos_to_message(chats: list[ChatListItemDTO]) -> str:
+    return '\n'.join(
+        (
+            'List of all avaible chats:',
+            '\n'.join(
+                (f"ChatOID: `{escape_markdown(chat.oid, version=2)}` Problem: {chat.title}" for chat in chats)  # type: ignore
+            )
+        )
+    )
